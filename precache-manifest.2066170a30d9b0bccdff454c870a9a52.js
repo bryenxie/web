2569c685fac47c15adb73240a450c6cb13c97839
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-04 16:18:54 +0000

    Deploy uSober/web to github.com/uSober/web.git:gh-pages

---
+++ precache-manifest.2066170a30d9b0bccdff454c870a9a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7bb2106d2bd8c16c72",
+    "url": "/web/static/css/main.74c80f8f.chunk.css"
+  },
+  {
+    "revision": "af7bb2106d2bd8c16c72",
+    "url": "/web/static/js/main.af7bb210.chunk.js"
+  },
+  {
+    "revision": "32b45f9052973d0172a2",
+    "url": "/web/static/css/1.f23ab3e2.chunk.css"
+  },
+  {
+    "revision": "32b45f9052973d0172a2",
+    "url": "/web/static/js/1.32b45f90.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "662ed8907fbae5dff90f998c03d8027c",
+    "url": "/web/index.html"
+  }
+];
